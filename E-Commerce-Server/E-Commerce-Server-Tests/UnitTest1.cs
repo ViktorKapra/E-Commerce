8fc807ac4737e7420339021493f34d3e5a1e42d6
Viktor Kapra <viktorkapra@gmail.com>
2024-10-17 13:48:50 +0300

    Configured test environment

---
+++ E-Commerce-Server/E-Commerce-Server-Tests/UnitTest1.cs
@@ -0,0 +1,32 @@
+namespace E_Commerce_Server_Tests
+{
+    public class UnitTest1
+    {
+        public int PlusFunction(int a, int b)
+        { return a + b; }
+        
+        [Fact]
+        public void Test_PlusFunction_Zero_Zero()
+        {
+            //Arrange
+            int a = 0;
+            int b = 0;
+            // Act
+            int result = PlusFunction(a, b);
+            //Asert
+            Assert.Equal(0, result);    
+        }
+
+        [Fact]
+        public void Failing_Test_PlusFunction()
+        {
+            //Arrange
+            int a = 0;
+            int b = 0;
+            // Act
+            int result = PlusFunction(a, b);
+            //Asert
+            Assert.Equal(1, result);
+        }
+    }
+}
