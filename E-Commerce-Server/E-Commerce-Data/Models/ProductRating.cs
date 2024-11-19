4d8cd436f559775bd5f6269de8a8f025e11574ff
Viktor Kapra <viktorkapra@gmail.com>
2024-11-19 15:47:45 +0200

    Added Action FIlter for Product filter request.  Added filtering functionality for products

---
+++ E-Commerce-Server/E-Commerce-Data/Models/ProductRating.cs
@@ -0,0 +1,16 @@
+﻿using ECom.Data.Account;
+using ECom.Data.Interfaces;
+
+namespace ECom.Data.Models
+{
+    public class ProductRating : ISoftDeletable
+    {
+
+        public int ProductId { get; set; }
+        public Guid UserId { get; set; }
+        public int Rating { get; set; }
+        public Product Product { get; set; }
+        public EComUser User { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
